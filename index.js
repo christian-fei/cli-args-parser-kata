2ca4f792279194d0a58caa25351fccf0f07e79e0
Christian Fei <crifei93@gmail.com>
2018-07-27 14:44:49 +0200

    refactor: extract methods isFlag, nextIsValue and nextIsNumber

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 module.exports = function cliArgsParser (args) {
+  const isFlag = arg => !arg.startsWith('--')
+  const nextIsValue = next => next && next.startsWith && !next.startsWith('--')
+  const nextIsNumber = next => parseInt(next) === +next
+
   return args.reduce((asObject, arg, index) => {
     const next = args[index + 1]
     const key = arg.replace(/^--/, '')
-    if (!arg.startsWith('--')) return asObject
-    if (next && next.startsWith && !next.startsWith('--')) return Object.assign(asObject, {[key]: next})
-    if (parseInt(next) === +next) return Object.assign(asObject, {[key]: parseInt(next)})
+    if (isFlag(arg)) return asObject
+    if (nextIsValue(next)) return Object.assign(asObject, {[key]: next})
+    if (nextIsNumber(next)) return Object.assign(asObject, {[key]: parseInt(next)})
     return Object.assign(asObject, {[key]: true})
   }, {})
 }
