d56acbd2a082b84d6c80e900fb017ab921580741
Christian Fei <crifei93@gmail.com>
2018-07-27 14:51:51 +0200

    refactor: reuse isFlag to value

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 module.exports = function argsToObject (args) {
   const isFlag = arg => arg && !arg.startsWith('--')
-  const nextIsValue = value => value && !value.startsWith('--')
   const nextIsNumber = value => parseInt(value) === +value
 
   return args.reduce((asObject, arg, index) => {
     const value = args[index + 1]
     const key = arg.replace(/^--/, '')
     if (isFlag(arg)) return asObject
-    if (nextIsValue(value)) return Object.assign(asObject, {[key]: value})
+    if (isFlag(value)) return Object.assign(asObject, {[key]: value})
     if (nextIsNumber(value)) return Object.assign(asObject, {[key]: parseInt(value)})
     return Object.assign(asObject, {[key]: true})
   }, {})
