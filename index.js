c1b65b6567ca5ee53d2b301cbd3234a7bdf879b1
Christian Fei <crifei93@gmail.com>
2018-07-27 14:54:03 +0200

    fix: rename to isValue

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const isFlag = arg => arg && !arg.startsWith('--')
+const isValue = arg => arg && !arg.startsWith('--')
 const isNumber = value => parseInt(value) === +value
 
 module.exports = function argsToObject (args) {
   return args.reduce((asObject, arg, index) => {
     const value = args[index + 1]
     const key = arg.replace(/^--/, '')
-    if (isFlag(arg)) return asObject
-    if (isFlag(value)) return Object.assign(asObject, {[key]: value})
+    if (isValue(arg)) return asObject
+    if (isValue(value)) return Object.assign(asObject, {[key]: value})
     if (isNumber(value)) return Object.assign(asObject, {[key]: parseInt(value)})
     return Object.assign(asObject, {[key]: true})
   }, {})
