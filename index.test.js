87eed4ab3726a634a4b939d82e552717e1b7bcc4
Christian Fei <crifei93@gmail.com>
2018-07-31 21:13:12 +0200

    make "try to support both `string` and `array` input formats" test pass

--- index.test.js
+++ index.test.js
@@ -36,7 +36,9 @@ test('try to support both `string` and `array` input formats', () => {
   })
 })
 
-function parseArgs ([arg, ...args], result = {}, flag = undefined) {
+function parseArgs (args, result = {}, flag = undefined) {
+  if (typeof args === 'string') args = JSON.parse(args)
+  const [arg] = args.splice(0, 1)
   if (arg && arg.startsWith && arg.startsWith('--')) {
     flag = arg.slice(2)
     return parseArgs(args, Object.assign(result, {[flag]: true}), flag)
